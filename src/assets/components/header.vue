97d47cc725be563797a9497e38c2181dd0d903c6
Vaibhav Bhawalkar <22888661+vbhv98@users.noreply.github.com>
2019-02-22 10:52:07 +0530

    styled home page
    images can be replaced with any other images we want

--- src/assets/components/header.vue
+++ src/assets/components/header.vue
@@ -1,44 +1,121 @@
 <template>
-  <div id="app-header">
-    <nav class="navbar navbar-light bg-secondary sticky-top">
-      <div class="container-fluid">
-      <router-link to="/" class="navbar-brand bg-primary">Sportify</router-link >
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-      </button>
-        <router-link class="nav-item nav-link active bg-primary" to="/">Home <span class="sr-only">(current)</span></router-link>
-        <a class="nav-item nav-link active bg-primary" href="#">Archives</a>
-        <a class="nav-item nav-link active bg-primary" href="#">Disabled</a>
-        <router-link class="nav-item nav-link active bg-primary" to="/aboutUs">About us</router-link>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-      <button class="btn bg-primary" type="submit">Search</button>
-    </form>
+  <nav id="app-header" class="header">
+    <div class="nav-item nav-home">
+      <router-link to="/" class="text">Sportify</router-link>
     </div>
-    </nav>
-
-  </div>
+    <div class="menu-btn">
+      <button v-on:click="displayMenu = !displayMenu">☰</button>
+    </div>
+    <ul class="nav-list" v-bind:class="{ active: displayMenu }">
+      <li class="nav-item current">
+        <router-link class to="/">Home</router-link>
+      </li>
+      <li class="nav-item"><router-link class to="/">Archives</router-link></li>
+      <li class="nav-item"><router-link class to="/">Disabled</router-link></li>
+      <li class="nav-item">
+        <router-link class to="/aboutUs">About us</router-link>
+      </li>
+      <li class="nav-item">
+        <form class>
+          <input class type="text" placeholder="Search" aria-label="Search" />
+          <button class type="submit">Search</button>
+        </form>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-
-    }
-  }
+  data: () => ({
+    displayMenu: false
+  })
 }
 </script>
 <style scoped>
-  .navbar-brand{
-    border-radius:5px;
-    padding:15px;
-  },
-  .nav-item{
-    color:black;
-    border-radius:5px;
-    padding:5px;
-    padding-left: 8px;
-    padding-right: 8px;
+.header {
+  display: flex;
+  height: 60px;
+}
+.nav-list {
+  flex: 1;
+  overflow: hidden;
+  display: flex;
+  list-style: none;
+  padding: 0 40px;
+  height: 60px;
+  justify-content: space-evenly;
+  align-items: center;
+  background: black;
+}
+.menu-btn {
+  width: 60px;
+  height: 60px;
+  display: none;
+}
+.menu-btn button {
+  width: 100%;
+  height: 100%;
+  border: none;
+  background: black;
+  color: deepskyblue;
+  font-size: 2em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+@media screen and (max-width: 930px) {
+  .header {
+    flex-wrap: wrap;
   }
+  .nav-list {
+    display: none;
+  }
+  .menu-btn {
+    display: block;
+    order: -1;
+  }
+  .nav-home {
+    flex: 0 0 calc(100% - 60px);
+  }
+}
+.nav-item {
+  padding: 10px 15px;
+  min-width: 80px;
+  text-align: center;
+}
+.nav-item a {
+  color: white;
+  text-decoration: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+}
+.current {
+  background: deepskyblue;
+}
+.nav-item:hover {
+  background: deepskyblue;
+}
+.nav-item:last-child:hover {
+  background: none;
+}
+.nav-home {
+  padding: 10px 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: deepskyblue;
+  width: 20%;
+}
+.active {
+  display: flex;
+  flex-direction: column;
+  padding-top: 1em;
+  flex: none;
+  height: calc(100vh - 60px);
+  z-index: 999;
+  max-width: 100%;
+}
 </style>
